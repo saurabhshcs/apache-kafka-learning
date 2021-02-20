be914f2a1293ab30884138b5d99ab01a3977d80c
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-02-20 01:59:42 +0000

    test commit

---
+++ library-producer-api/src/main/java/com/techsharezone/library/producer/api/LibraryProducerApiApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.library.producer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProducerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProducerApiApplication.class, args);
+	}
+
+}
---
+++ library-producer-api/src/test/java/com/techsharezone/library/producer/api/LibraryProducerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.library.producer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProducerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9180115fc0219843ca80933d390b2b3314fc58c0
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-02-27 01:02:57 +0000

    [TSZ-001] - Updated application.yaml and created Book domain.

---
+++ library-producer-api/src/main/java/com/techsharezone/library/producer/api/domain/Book.java
@@ -0,0 +1,18 @@
+package com.techsharezone.library.producer.api.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Book {
+
+    private Integer id;
+    private String name;
+    private String author;
+}

f957e90499d2e6af268cdef108b15c2df05420a0
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-03-02 23:23:23 +0000

    [TSZ-007] - Rename LibraryEventControllerIntegratioTest amd added embeded kafka configurations

--- library-producer-api/src/test/java/integrationtests/com/techsharezone/library/producer/api/controller/LibraryEventControllerIntegrationTest.java
+++ library-producer-api/src/test/java/integrationtests/com/techsharezone/library/producer/api/controller/LibraryEventControllerIntegrationTest.java
@@ -7,16 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class LibraryEventControllerTest {
+@ActiveProfiles({"test", "kafka"})
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+class LibraryEventControllerIntegrationTest {
 
     @Autowired
     TestRestTemplate testRestTemplate;
+
 
     @Test
     void postLibraryEvent() {

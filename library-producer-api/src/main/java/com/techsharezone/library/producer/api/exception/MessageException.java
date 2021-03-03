3ffbff65674ca97aa9cf793a78aa3bd8cec17df1
SAURABH SHARMA <saurabhshcs@gmail.com>
2021-03-03 23:36:33 +0000

    Update MessageException.java

--- library-producer-api/src/main/java/com/techsharezone/library/producer/api/exception/MessageException.java
+++ library-producer-api/src/main/java/com/techsharezone/library/producer/api/exception/MessageException.java
@@ -1,4 +1,9 @@
 package com.techsharezone.library.producer.api.exception;
 
+/*
+ * @project library-producer
+ * @author  saurabhshcs
+ */
+
 public class MessageException extends RuntimeException{
 }

06b528f7d9d697147305a0cad324e61bb4b0a586
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-03-03 00:11:41 +0000

    [TSZ-001- Consumer-API setup]

---
+++ library-event-consumer-api/src/main/java/com/techsharezone/libraryeventconsumerapi/LibraryEventConsumerApiApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.libraryeventconsumerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventConsumerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventConsumerApiApplication.class, args);
+	}
+
+}
---
+++ library-event-consumer-api/src/test/java/com/techsharezone/libraryeventconsumerapi/LibraryEventConsumerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.libraryeventconsumerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventConsumerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

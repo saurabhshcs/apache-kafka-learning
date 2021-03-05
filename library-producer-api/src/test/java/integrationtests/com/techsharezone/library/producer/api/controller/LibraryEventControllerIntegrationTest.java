4124fc0d0af99d8a3190963026afb5e51fba6c02
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-03-05 01:03:32 +0000

    [TSZ-008] - Added new method for PUT request and need to fix an error for consumer instance.

--- library-producer-api/src/test/java/integrationtests/com/techsharezone/library/producer/api/controller/LibraryEventControllerIntegrationTest.java
+++ library-producer-api/src/test/java/integrationtests/com/techsharezone/library/producer/api/controller/LibraryEventControllerIntegrationTest.java
@@ -2,16 +2,20 @@ package com.techsharezone.library.producer.api.controller;
 
 import com.techsharezone.library.producer.api.domain.Book;
 import com.techsharezone.library.producer.api.domain.LibraryEvent;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
  * @project library-producer
@@ -25,6 +29,9 @@ class LibraryEventControllerIntegrationTest {
 
     @Autowired
     TestRestTemplate testRestTemplate;
+
+    @Autowired
+    Libra
 
 
     @Test
@@ -76,4 +83,37 @@ class LibraryEventControllerIntegrationTest {
         //then
         assertThat(HttpStatus.CREATED, is(responseEntity.getStatusCode()));
     }
+
+    @Test
+    @Timeout(5)
+    void putLibraryEvent() throws InterruptedException {
+        //given
+        Book book = Book.builder()
+                .id(456)
+                .name("Apache Kafka in Action")
+                .author("Saurabh")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(321)
+                .book(book)
+                .build();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
+
+        //when
+        ResponseEntity<LibraryEvent> responseEntity = testRestTemplate.exchange("/v1/libraryevent", HttpMethod.PUT, request, LibraryEvent.class);
+
+        //then
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        ConsumerRecord<Integer, String> consumerRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+        //Thread.sleep(3000);
+        String expectedRecord = "{\"libraryEventId\":123,\"libraryEventType\":\"UPDATE\",\"book\":{\"bookId\":456,\"bookName\":\"Kafka using Spring Boot\",\"bookAuthor\":\"Dilip\"}}";
+        String value = consumerRecord.value();
+        assertEquals(expectedRecord, value);
+
+    }
+
 }
